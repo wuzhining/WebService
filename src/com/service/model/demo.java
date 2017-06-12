8de38a033b44e54e92ff3eb311ab131577e5bbeb
wuzhining <354753575@qq.com>
2017-06-12 16:02:29 +0800

    2017.6.12 16:02

--- src/com/service/model/demo.java
+++ src/com/service/model/demo.java
@@ -1,27 +1,49 @@
 package com.service.model;
 
 import java.lang.reflect.Field;
+import java.util.Map;
 
 public class demo {
-		public Object getService(String usrName,String usrPwd){
+		public Object getServiceObject(String usrName,String usrPwd){
 			jsonObject jo=new jsonObject();
 			jo.setUsrName(usrName);
 			jo.setPwdWord(usrPwd);
-			
 			return  jo;
 		}
-		public static void main(String[] args) {
+		public String getService(String str){
+			return "Best Wish "+str+"!";
+		}
+		public String getServiceAgian(String str){
+			return "Best Wish "+str+"!";
+		}
+		
+		@SuppressWarnings("null")
+		public Map<String,String> getJson(String name,String value){
+			Map<String, String> map = null;
+			map.put(name, value);
+			return  map;
+		}
+		public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException {
 			demo dm=new demo();
-			Object ob= dm.getService("大宝", "二宝");
-			System.out.println(ob.toString());
-			
+			jsonObject ob= (jsonObject) dm.getServiceObject("大宝", "二宝");
+			System.out.println(ob.getClass().getName());
+			String name=null;
+			Object value=null;
 			try {
-				Field receiName=ob.getClass().getField("usrName");
-			} catch (NoSuchFieldException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				Field[] fields=ob.getClass().getDeclaredFields();
+				for (int i = 0; i < fields.length; i++) {
+					Field example=fields[i];
+					example.setAccessible(true);
+					name=example.getName();
+					value=example.get(ob);
+					System.out.println(name+":"+value);
+					System.out.println(dm.getServiceAgian(value.toString()));
+				}
+				System.out.println(dm.getServiceAgian("尼古拉斯赵四"));
+				
+				Map<String, String> map = dm.getJson("Str", "二宝");
+				System.out.println(map.toString());
 			} catch (SecurityException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
--- src/com/service/model/service.java
+++ src/com/service/model/service.java
@@ -2,6 +2,6 @@ package com.service.model;
 
 public class service {
 		public String getService(String str){
-			return "Hi,"+str;
+			return "Hi,你好，"+str;
 		}
 }
